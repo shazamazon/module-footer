f8a405ea40e70507e550de6f73a7a223f4c99f58
jkcryptolock <jkconno@gmail.com>
2019-09-06 12:28:33 -0500

    more small changes

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div id="footer">
-          <div onClick={window.scrollTo(0, 0)} id="footer-top">
+          <div onClick={() => window.scrollTo(0, 0)} id="footer-top">
               <span id="footer-toptext">Back to top</span>
           </div>
           <div id="footer-middle">
@@ -16,58 +16,58 @@ class App extends React.Component {
                   <div id="footer-middleCols">
                       <div className="footer-row">
                           <div className="footer-rowtitle">Get to Know Us</div>
-                          <ul className="footer-ul">
-                              <li className="footer-list">Careers</li>
-                              <li className="footer-list">Blog</li>
-                              <li className="footer-list">About Shazamazon</li>
-                              <li className="footer-list">Press Center</li>
-                              <li className="footer-list">Investor Relations</li>
-                              <li className="footer-list">Shazamazon Devices</li>
-                              <li className="footer-list">Shazamazon Tours</li>
-                          </ul>
+                            <ul className="footer-ul">
+                                <li className="footer-list">Careers</li>
+                                <li className="footer-list">Blog</li>
+                                <li className="footer-list">About Shazamazon</li>
+                                <li className="footer-list">Press Center</li>
+                                <li className="footer-list">Investor Relations</li>
+                                <li className="footer-list">Shazamazon Devices</li>
+                                <li className="footer-list">Shazamazon Tours</li>
+                            </ul>
                       </div>
                       <div className="footer-spacer"></div>
-                      <div className="footer-row">
-                          <div className="footer-rowtitle">Make Money with Us</div>
-                          <ul className="footer-ul">
-                            <li className="footer-list">Sell on Shazamazon</li>
-                            <li className="footer-list">Sell Under Private Brands</li>
-                            <li className="footer-list">Sell on Shazamazon Handmade</li>
-                            <li className="footer-list">Sell Your Services on Shazamazon</li>
-                            <li className="footer-list">Sell on Shazamazon Business</li>
-                            <li className="footer-list">Sell Your Apps on Shazamazon</li>
-                            <li className="footer-list">Become an Affiliate</li>
-                            <li className="footer-list">Advertise Your Products</li>
-                            <li className="footer-list">Self-Publish with Us</li>
-                          </ul>
-                      </div>
+                        <div className="footer-row">
+                            <div className="footer-rowtitle">Make Money with Us</div>
+                                <ul className="footer-ul">
+                                    <li className="footer-list">Sell on Shazamazon</li>
+                                    <li className="footer-list">Sell Under Private Brands</li>
+                                    <li className="footer-list">Sell on Shazamazon Handmade</li>
+                                    <li className="footer-list">Sell Your Services on Shazamazon</li>
+                                    <li className="footer-list">Sell on Shazamazon Business</li>
+                                    <li className="footer-list">Sell Your Apps on Shazamazon</li>
+                                    <li className="footer-list">Become an Affiliate</li>
+                                    <li className="footer-list">Advertise Your Products</li>
+                                    <li className="footer-list">Self-Publish with Us</li>
+                                </ul>
+                        </div>
                       <div className="footer-spacer"></div>
-                      <div className="footer-row">
-                          <div className="footer-rowtitle">Shazamazon Payment Products</div>
-                          <ul>
-                            <li className="footer-list">Shazamazon Rewards Visa Signature Cards</li>
-                            <li className="footer-list">Shazamazon.com Store Card</li>
-                            <li className="footer-list">Shazamazon Business Card</li>
-                            <li className="footer-list">Shazamazon.com Corporate Credit Line</li>
-                            <li className="footer-list">Shop with Points</li>
-                            <li className="footer-list">Credit Card Marketplace</li>
-                            <li className="footer-list">Reload Your Balance</li>
-                            <li className="footer-list">Shazamazon Currency Converter</li>
-                          </ul>
-                      </div>
+                        <div className="footer-row">
+                            <div className="footer-rowtitle">Shazamazon Payment Products</div>
+                                <ul className="footer-ul">
+                                    <li className="footer-list">Shazamazon Rewards Visa Signature Cards</li>
+                                    <li className="footer-list">Shazamazon.com Store Card</li>
+                                    <li className="footer-list">Shazamazon Business Card</li>
+                                    <li className="footer-list">Shazamazon.com Corporate Credit Line</li>
+                                    <li className="footer-list">Shop with Points</li>
+                                    <li className="footer-list">Credit Card Marketplace</li>
+                                    <li className="footer-list">Reload Your Balance</li>
+                                    <li className="footer-list">Shazamazon Currency Converter</li>
+                                </ul>
+                        </div>
                       <div className="footer-spacer"></div>
                       <div className="footer-row">
                           <div className="footer-rowtitle">Let Us Help You</div>
-                          <ul className="footer-ul">
-                            <li className="footer-list">Your Account</li>
-                            <li className="footer-list">Your Orders</li>
-                            <li className="footer-list">Shipping Rates & Policies</li>
-                            <li className="footer-list">Shazamazon Prime</li>
-                            <li className="footer-list">Returns & Replacements</li>
-                            <li className="footer-list">Manage Your Content and Devices</li>
-                            <li className="footer-list">Shazamazon Assistant</li>
-                            <li className="footer-list">Help</li>
-                          </ul>
+                            <ul className="footer-ul">
+                                <li className="footer-list">Your Account</li>
+                                <li className="footer-list">Your Orders</li>
+                                <li className="footer-list">Shipping Rates & Policies</li>
+                                <li className="footer-list">Shazamazon Prime</li>
+                                <li className="footer-list">Returns & Replacements</li>
+                                <li className="footer-list">Manage Your Content and Devices</li>
+                                <li className="footer-list">Shazamazon Assistant</li>
+                                <li className="footer-list">Help</li>
+                            </ul>
                       </div>
                   </div>
               </div>
